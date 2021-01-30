4fcd38a693459864495c5d1c074d8bf075abced7
jofravimusic <jofravimusic@byui.edu>
2021-01-30 15:55:41 -0300

    Adding a class to the project

---
+++ src/Prove04/HelloTest.java
@@ -0,0 +1,12 @@
+package Prove04;
+
+import java.util.Scanner;
+
+public class HelloTest {
+
+    public static void main(String[] args){
+        System.out.println("Enter you name:");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi, "+sc.nextLine());
+    }
+}
